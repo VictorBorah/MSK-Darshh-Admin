07117c09a9c30cecee3f120a67833771a054d437
Victor Borah <victorborah@gmail.com>
2026-03-23 13:56:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // This tells Next.js to create the /out folder
+  trailingSlash: true, // Forces `/projects` to output as `/projects/index.html` instead of `/projects.html` to prevent server directory indexing
   images: {
     unoptimized: true, // Required for static exports using Next <Image> tags
   },
